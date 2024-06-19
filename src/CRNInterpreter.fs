340a6b8ec0ff827acb08bdba006c08f51341b838
Jonathan <jonathan.hoejlev@gmail.com>
2024-06-19 13:22:45 +0200

    CRNTypecheck

--- src/CRNInterpreter.fs
+++ src/CRNInterpreter.fs
@@ -1,204 +1,73 @@
 module CRNInterpreter
 
 open CRNpp
+open CRNTypecheck
 
-type State = Map<species,float>
-// type TypecheckState = Map<species, species> * (bool * bool * bool * bool * bool) * Set<species> // Adj list, bool for each if case, Written-to species
-let mapAppend map key value =
-    Map.change
-        key
-        (function
-        | None -> Some([ value ])
-        | Some(l) -> Some(value :: l))
-        map
-
-let isIf (com: Command) = 
-    match com with
-    | IfGT(_) -> true
-    | IfGE(_) -> true
-    | IfEQ(_) -> true
-    | IfLT(_) -> true
-    | IfLE(_) -> true
-    | _ -> false
-
-
-let addOneToMap (map:Map<species, int>) (species:species) = 
-    Map.change
-        species
-        (function
-        | None -> Some(1)
-        | Some(i) -> Some(i+1))
-        map
-    
-
-let subOneOfMap (map:Map<species, int>) (species:species) = 
-    Map.change
-        species
-        (function
-        | None -> Some(0)
-        | Some(i) -> Some(i-1))
-        map
-
-
-let rec isCycleFree (heapIsh: Map<int,species Set>) (inArrows: Map<species,int>) = 
-    if heapIsh.IsEmpty then
-        true
-    else
-        if heapIsh.ContainsKey(0) && not (Map.find 0 heapIsh |> List.isEmpty) then
-            let headSpecies = Map.find 0 heapIsh |> fst
-            let inArrows' = List.fold (fun h s -> subOneOfMap h s) heapIsh (Map.find headSpecies inArrows)
-            //isCycleFree heapish' inArrows
-        else
-            false
-
-
-let isAsyclic (adj: Map<species, List<species>>) = 
-    let keys = (adj.Keys |> Seq.cast |> Set.ofSeq)
-    let values = (List.concat adj.Values |> Set.ofList)
-    let allSpecies = Set.union keys values
-    let noOnePointsTo = Set.
-    isCycleFree (Map.fold (fun m k v-> Map.add k (v |> Seq.cast |> Set.ofSeq) m) Map.empty heapIsh) inArrows
-
-
-let isTyped (R(concs, steps): Root) : bool =
-    let concsPass =
-        List.fold
-            (fun (b, M) (C(sp, num)) -> ((b && not (Set.contains sp M)) && num >= 0, Set.add sp M))
-            (true, Set.empty)
-            concs
-        |> fst
-
-    let rec stepCheck
-        (cmds: Command list)
-        (adj: Map<species, List<species>>)
-        (b1, b2, b3, b4, b5)
-        (writtenTo: Set<species>)
-        =
-        match cmds with
-        | Ld(r, p) :: cs ->
-            let adj' = mapAppend adj r p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Add(r1, r2, p) :: cs ->
-            let adj' = mapAppend adj r1 p
-            let adj'' = mapAppend adj' r2 p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Sub(r1, r2, p) :: cs ->
-            let adj' = mapAppend adj r1 p
-            let adj'' = mapAppend adj' r2 p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Mul(r1, r2, p) :: cs ->
-            let adj' = mapAppend adj r1 p
-            let adj'' = mapAppend adj' r2 p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Div(r1, r2, p) :: cs ->
-            let adj' = mapAppend adj r1 p
-            let adj'' = mapAppend adj' r2 p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Sqrt(r, p) :: cs ->
-            let adj' = mapAppend adj r p
-            not (writtenTo.Contains p)
-            && stepCheck cs adj' (b1, b2, b3, b4, b5) (writtenTo.Add p)
-        | Cmp(_, _) :: cs -> 
-            not (writtenTo.Contains "cmp")
-            && stepCheck cs adj (b1, b2, b3, b4, b5) (writtenTo.Add "cmp")
-        | Rx(reac, prodreacs, num) :: cs -> 
-            let prod = Set.difference (Set.ofList prodreacs) (Set.ofList reac)
-            let adj' = List.fold (fun m r -> Set.fold (fun m' p -> mapAppend m' r p) m prod) adj reac
-            let productsAreNew = Set.intersect prod writtenTo |> Set.isEmpty
-            let writtenTo' = Set.union writtenTo prod
-            num >= 0 && productsAreNew && stepCheck cs adj' (b1, b2, b3, b4, b5) writtenTo'
-        | IfGT(com) :: cs -> 
-            if List.forall (fun c -> isIf c) cs then
-                not b1 && not b2 && stepCheck com adj (true, true, true, true, true) writtenTo && stepCheck cs adj (true, b2, b3, b4, b5) writtenTo
-            else stepCheck (cs @ [IfGT(com)]) adj (b1, b2, b3, b4, b5) (writtenTo)
-        | IfGE(com) :: cs -> 
-            if List.forall (fun c -> isIf c) cs then
-                not b1 && not b2 && not b3 && not b5 && stepCheck com adj (true, true, true, true, true) writtenTo && stepCheck cs adj (b1, true, b3, b4, b5) writtenTo
-            else stepCheck (cs @ [IfGE(com)]) adj (b1, b2, b3, b4, b5) (writtenTo)
-        | IfEQ(com) :: cs -> 
-            if List.forall (fun c -> isIf c) cs then
-                not b2 && not b3 && not b5 && stepCheck com adj (true, true, true, true, true) writtenTo && stepCheck cs adj (b1, b2, true, b4, b5) writtenTo
-            else stepCheck (cs @ [IfEQ(com)]) adj (b1, b2, b3, b4, b5) (writtenTo)
-        | IfLT(com) :: cs -> 
-            if List.forall (fun c -> isIf c) cs then
-                not b4 && not b5 && stepCheck com adj (true, true, true, true, true) writtenTo && stepCheck cs adj (b1, b2, b3, true, b5) writtenTo
-            else stepCheck (cs @ [IfLT(com)]) adj (b1, b2, b3, b4, b5) (writtenTo)
-        | IfLE(com) :: cs -> 
-            if List.forall (fun c -> isIf c) cs then
-                not b2 && not b3 && not b4 && not b5 && stepCheck com adj (true, true, true, true, true) writtenTo && stepCheck cs adj (b1, b2, b3, b4, true) writtenTo
-            else stepCheck (cs @ [IfLE(com)]) adj (b1, b2, b3, b4, b5) (writtenTo)
-        | [] -> isAsyclic adj
+type State = Map<species, float>
 
 
 
-    // | Ld of species * species
-    // | Add of species * species * species
-    // | Sub of species * species * species
-    // | Mul of species * species * species
-    // | Div of species * species * species
-    // | Sqrt of species * species
-    // | Cmp of species * species
-    // | Rx of species List * species List * float
-    // | IfGT of Command List
-    // | IfGE of Command List
-    // | IfEQ of Command List
-    // | IfLT of Command List
-    // | IfLE of Command List
+let getInitialState (concl: Conc List) : State =
+    List.fold (fun state (C(s, c)) -> Map.add s c state) Map.empty concl
 
-    let stepsPass =
-        List.fold
-            (fun b (S(cmds)) -> b && stepCheck cmds (Map.empty) (false, false, false, false, false) Set.empty)
-            true
-            steps
+let getValue (s: species) (state: State) =
+    if Map.containsKey s state then Map.find s state else 0
 
-    concsPass && stepsPass
+let eqCheck x y = abs (x - y) <= 0.5
 
-
-
-
-let getInitialState (concl : Conc List) : State = List.fold (fun state (C(s,c)) -> Map.add s c state) Map.empty concl
-
-let getValue (s : species) (state : State) = if Map.containsKey s state then Map.find s state else 0
-
-let eqCheck x y = abs (x-y) <= 0.5
-
-let rec doCommandList (cl : Command List) (state : State) = 
+let rec doCommandList (cl: Command List) (state: State) =
     let getState s = getValue s state
+
     match cl with
     | [] -> state
-    | Ld(x,y)::clt -> doCommandList clt (Map.add y (getState x) state) 
-    | Add(x,y,z)::clt -> doCommandList clt (Map.add z ((getState x) + (getState y)) state)
-    | Sub(x,y,z)::clt -> doCommandList clt (Map.add z ((getState x) - (getState y)) state)
-    | Mul(x,y,z)::clt -> doCommandList clt (Map.add z ((getState x) * (getState y)) state)
-    | Div(x,y,z)::clt -> doCommandList clt (Map.add z ((getState x) / (getState y)) state)
-    | Sqrt(x,y)::clt -> doCommandList clt (Map.add y (sqrt (getState x)) state)
-    | Cmp(x,y)::clt ->  let xysum = (getState x) + (getState y)
-                        let Xgt,Xlt = (getState x) / xysum, (getState y) / xysum
-                        let XltState = Map.add "Xlt" Xlt state
-                        let XgtState = Map.add "Xgt" Xgt XltState
-                        doCommandList clt XgtState
-    | Rx(p,r,c)::clt -> failwith "Cannot interpret custom reactions!"
-    | IfGT(cmdl)::clt -> if getState "Xgt" > getState "Xlt" then doCommandList clt (doCommandList cmdl state) else doCommandList clt state
-    | IfGE(cmdl)::clt -> if getState "Xgt" > getState "Xlt" || eqCheck (getState "Xgt") (getState "Xlt") then doCommandList clt (doCommandList cmdl state) else doCommandList clt state                    
-    | IfEQ(cmdl)::clt -> if eqCheck (getState "Xgt") (getState "Xlt") then doCommandList clt (doCommandList cmdl state) else doCommandList clt state                    
-    | IfLT(cmdl)::clt -> if getState "Xgt" < getState "Xlt" then doCommandList clt (doCommandList cmdl state) else doCommandList clt state
-    | IfLE(cmdl)::clt -> if getState "Xgt" < getState "Xlt" || eqCheck (getState "Xgt") (getState "Xlt") then doCommandList clt (doCommandList cmdl state) else doCommandList clt state                    
+    | Ld(x, y) :: clt -> doCommandList clt (Map.add y (getState x) state)
+    | Add(x, y, z) :: clt -> doCommandList clt (Map.add z ((getState x) + (getState y)) state)
+    | Sub(x, y, z) :: clt -> doCommandList clt (Map.add z ((getState x) - (getState y)) state)
+    | Mul(x, y, z) :: clt -> doCommandList clt (Map.add z ((getState x) * (getState y)) state)
+    | Div(x, y, z) :: clt -> doCommandList clt (Map.add z ((getState x) / (getState y)) state)
+    | Sqrt(x, y) :: clt -> doCommandList clt (Map.add y (sqrt (getState x)) state)
+    | Cmp(x, y) :: clt ->
+        let xysum = (getState x) + (getState y)
+        let Xgt, Xlt = (getState x) / xysum, (getState y) / xysum
+        let XltState = Map.add "Xlt" Xlt state
+        let XgtState = Map.add "Xgt" Xgt XltState
+        doCommandList clt XgtState
+    | Rx(p, r, c) :: clt -> failwith "Cannot interpret custom reactions!"
+    | IfGT(cmdl) :: clt ->
+        if getState "Xgt" > getState "Xlt" then
+            doCommandList clt (doCommandList cmdl state)
+        else
+            doCommandList clt state
+    | IfGE(cmdl) :: clt ->
+        if getState "Xgt" > getState "Xlt" || eqCheck (getState "Xgt") (getState "Xlt") then
+            doCommandList clt (doCommandList cmdl state)
+        else
+            doCommandList clt state
+    | IfEQ(cmdl) :: clt ->
+        if eqCheck (getState "Xgt") (getState "Xlt") then
+            doCommandList clt (doCommandList cmdl state)
+        else
+            doCommandList clt state
+    | IfLT(cmdl) :: clt ->
+        if getState "Xgt" < getState "Xlt" then
+            doCommandList clt (doCommandList cmdl state)
+        else
+            doCommandList clt state
+    | IfLE(cmdl) :: clt ->
+        if getState "Xgt" < getState "Xlt" || eqCheck (getState "Xgt") (getState "Xlt") then
+            doCommandList clt (doCommandList cmdl state)
+        else
+            doCommandList clt state
 
 
 let doStep (S(cl): Step) (state: State) : State = doCommandList cl state
 
 
-let interpretProgram (R(concl,stepl)) =
+let interpretProgram (R(concl, stepl)) =
     let initialState = getInitialState concl
-    Seq.unfold (fun (state, i) -> 
-                    let nextState = doStep (List.item (i % List.length stepl) stepl) state
-                    Some(nextState,(nextState,i+1))) (initialState,0)
 
-
-
-
+    Seq.unfold
+        (fun (state, i) ->
+            let nextState = doStep (List.item (i % List.length stepl) stepl) state
+            Some(nextState, (nextState, i + 1)))
+        (initialState, 0)
---
+++ src/CRNTypecheck.fs
@@ -0,0 +1,186 @@
+module CRNTypecheck
+
+open CRNpp
+
+let mapAppend map key value =
+    Map.change
+        key
+        (function
+        | None -> Some([ value ])
+        | Some(l) -> Some(value :: l))
+        map
+
+let isIf (com: Command) =
+    match com with
+    | IfGT(_) -> true
+    | IfGE(_) -> true
+    | IfEQ(_) -> true
+    | IfLT(_) -> true
+    | IfLE(_) -> true
+    | _ -> false
+
+
+let addOneToMap (map: Map<species, int>) (species: species) =
+    Map.change
+        species
+        (function
+        | None -> Some(1)
+        | Some(i) -> Some(i + 1))
+        map
+
+
+let subOneOfMap (map: Map<species, int>) (species: species) =
+    Map.change
+        species
+        (function
+        | None -> Some(0)
+        | Some(i) -> Some(i - 1))
+        map
+
+
+let isAsyclic (adj: Map<species, List<species>>) =
+    let keys = (adj.Keys |> Seq.cast |> Set.ofSeq)
+    let values = (List.concat adj.Values |> Set.ofList)
+    let allSpecies = Set.union keys values
+    let noOnePointsTo = Set.difference allSpecies values |> Set.toList
+
+    let inDegrees =
+        List.fold (fun M s -> addOneToMap M s) Map.empty (List.concat adj.Values)
+
+    let rec isDAG q inDegs =
+        match q with
+        | [] -> Map.isEmpty inDegs
+        | e :: es ->
+            let (D, Q) =
+                List.fold
+                    (fun (M, q') y ->
+                        let x = (Map.find y M)
+                        let M' = if x <= 1 then Map.remove y M else subOneOfMap M y
+                        let Q = if x <= 1 then y :: q' else q'
+                        (M', Q))
+                    (inDegs, es)
+                    (Map.find e adj)
+
+            isDAG Q D
+
+    isDAG noOnePointsTo inDegrees
+
+
+
+let isTyped (R(concs, steps): Root) : bool =
+    let concsPass =
+        List.fold
+            (fun (b, M) (C(sp, num)) -> ((b && not (Set.contains sp M)) && num >= 0, Set.add sp M))
+            (true, Set.empty)
+            concs
+        |> fst
+
+    let rec stepCheck
+        (cmds: Command list)
+        (adj: Map<species, List<species>>)
+        (b1, b2, b3, b4, b5)
+        (writtenTo: Set<species>)
+        =
+        match cmds with
+        | Ld(r, p) :: cs ->
+            let adj' = mapAppend adj r p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Add(r1, r2, p) :: cs ->
+            let adj' = mapAppend adj r1 p
+            let adj'' = mapAppend adj' r2 p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Sub(r1, r2, p) :: cs ->
+            let adj' = mapAppend adj r1 p
+            let adj'' = mapAppend adj' r2 p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Mul(r1, r2, p) :: cs ->
+            let adj' = mapAppend adj r1 p
+            let adj'' = mapAppend adj' r2 p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Div(r1, r2, p) :: cs ->
+            let adj' = mapAppend adj r1 p
+            let adj'' = mapAppend adj' r2 p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj'' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Sqrt(r, p) :: cs ->
+            let adj' = mapAppend adj r p
+
+            not (writtenTo.Contains p)
+            && stepCheck cs adj' (b1, b2, b3, b4, b5) (writtenTo.Add p)
+        | Cmp(_, _) :: cs ->
+            not (writtenTo.Contains "cmp")
+            && stepCheck cs adj (b1, b2, b3, b4, b5) (writtenTo.Add "cmp")
+        | Rx(reac, prodreacs, num) :: cs ->
+            let prod = Set.difference (Set.ofList prodreacs) (Set.ofList reac)
+
+            let adj' =
+                List.fold (fun m r -> Set.fold (fun m' p -> mapAppend m' r p) m prod) adj reac
+
+            let productsAreNew = Set.intersect prod writtenTo |> Set.isEmpty
+            let writtenTo' = Set.union writtenTo prod
+            num >= 0 && productsAreNew && stepCheck cs adj' (b1, b2, b3, b4, b5) writtenTo'
+        | IfGT(com) :: cs ->
+            if List.forall (fun c -> isIf c) cs then
+                not b1
+                && not b2
+                && stepCheck com adj (true, true, true, true, true) writtenTo
+                && stepCheck cs adj (true, b2, b3, b4, b5) writtenTo
+            else
+                stepCheck (cs @ [ IfGT(com) ]) adj (b1, b2, b3, b4, b5) (writtenTo)
+        | IfGE(com) :: cs ->
+            if List.forall (fun c -> isIf c) cs then
+                not b1
+                && not b2
+                && not b3
+                && not b5
+                && stepCheck com adj (true, true, true, true, true) writtenTo
+                && stepCheck cs adj (b1, true, b3, b4, b5) writtenTo
+            else
+                stepCheck (cs @ [ IfGE(com) ]) adj (b1, b2, b3, b4, b5) (writtenTo)
+        | IfEQ(com) :: cs ->
+            if List.forall (fun c -> isIf c) cs then
+                not b2
+                && not b3
+                && not b5
+                && stepCheck com adj (true, true, true, true, true) writtenTo
+                && stepCheck cs adj (b1, b2, true, b4, b5) writtenTo
+            else
+                stepCheck (cs @ [ IfEQ(com) ]) adj (b1, b2, b3, b4, b5) (writtenTo)
+        | IfLT(com) :: cs ->
+            if List.forall (fun c -> isIf c) cs then
+                not b4
+                && not b5
+                && stepCheck com adj (true, true, true, true, true) writtenTo
+                && stepCheck cs adj (b1, b2, b3, true, b5) writtenTo
+            else
+                stepCheck (cs @ [ IfLT(com) ]) adj (b1, b2, b3, b4, b5) (writtenTo)
+        | IfLE(com) :: cs ->
+            if List.forall (fun c -> isIf c) cs then
+                not b2
+                && not b3
+                && not b4
+                && not b5
+                && stepCheck com adj (true, true, true, true, true) writtenTo
+                && stepCheck cs adj (b1, b2, b3, b4, true) writtenTo
+            else
+                stepCheck (cs @ [ IfLE(com) ]) adj (b1, b2, b3, b4, b5) (writtenTo)
+        | [] -> isAsyclic adj
+
+
+
+    let stepsPass =
+        List.fold
+            (fun b (S(cmds)) -> b && stepCheck cmds (Map.empty) (false, false, false, false, false) Set.empty)
+            true
+            steps
+
+    concsPass && stepsPass
