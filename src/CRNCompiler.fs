7297dbb8dcabd14beb9411c7c4afab0c074a3094
Jonathan <jonathan.hoejlev@gmail.com>
2024-06-21 14:22:05 +0200

    easy part of commandtoreacs

--- src/CRNCompiler.fs
+++ src/CRNCompiler.fs
@@ -5,19 +5,42 @@ open Reactions
 open ReactionsParser
 
 let listsToReaction l1 l2 =
-    Rxn(Map.ofList (List.map (fun x -> (x, 1.0)) l1), Map.ofList (List.map (fun x -> (x, 1.0)) l2), 1)
+    let f l =
+        Map.ofList (List.map (fun x -> (x, 1.0)) l)
+
+    Rxn(f l1, f l2, 1)
 
 
 let commandToReactions cmd subcnt =
     match cmd with
-    | Ld(x, y) -> [ listsToReaction [ x ] [ x; y ]; listsToReaction [ y ] [ "Ø" ] ]
-    | Add(x, y, z) -> []
-    | Sub(_, _, _) -> failwith "Not Implemented"
-    | Mul(_, _, _) -> failwith "Not Implemented"
-    | Div(_, _, _) -> failwith "Not Implemented"
-    | Sqrt(_, _) -> failwith "Not Implemented"
-    | Cmp(_, _) -> failwith "Not Implemented"
-    | Rx(_, _, _) -> failwith "Not Implemented"
+    | Ld(a, b) -> [ listsToReaction [ a ] [ a; b ]; listsToReaction [ b ] [ "Ø" ] ]
+    | Add(a, b, c) ->
+        [ listsToReaction [ a ] [ a; c ]
+          listsToReaction [ b ] [ b; c ]
+          listsToReaction [ c ] [ "Ø" ] ]
+    | Sub(a, b, c) ->
+        [ listsToReaction [ a ] [ a; c ]
+          listsToReaction [ b ] [ b; sprintf "H%A" subcnt ]
+          listsToReaction [ c ] [ "Ø" ]
+          listsToReaction [ c; sprintf "H%A" subcnt ] [ "Ø" ] ]
+    | Mul(a, b, c) -> [ listsToReaction [ a; b ] [ a; b; c ]; listsToReaction [ c ] [ "Ø" ] ]
+    | Div(a, b, c) -> [ listsToReaction [ a ] [ a; c ]; listsToReaction [ b; c ] [ b ] ]
+    | Sqrt(a, b) ->
+        [ listsToReaction [ a ] [ a; b ]
+          Rxn(Map.ofList [ (b, 1); (b, 1) ], Map.ofList [ ("Ø", 1.0) ], 0.5) ]
+    | Rx(l1, l2, k) ->
+        let f l =
+            Map.ofList (List.map (fun x -> (x, 1.0)) l)
+
+        [ Rxn(f l1, f l2, k) ]
+    | Cmp(x, y) ->
+        [ listsToReaction [ "Xgty"; y ] [ "Xlty"; y ]
+          listsToReaction [ "Xlty"; x ] [ "Xgty"; x ] // CRN 7
+          // Need to be seperated
+          listsToReaction [ "Xgyt"; "Xlty" ] [ "Xlty"; "CMP" ]
+          listsToReaction [ "Xgyt"; "Xlty" ] [ "Xgty"; "CMP" ]
+          listsToReaction [ "CMP"; "Xlty" ] [ "Xlty"; "Xlty" ]
+          listsToReaction [ "CMP"; "Xgty" ] [ "Xgty"; "Xgty" ] ] // CRN 8
     | IfGT(_) -> failwith "Not Implemented"
     | IfGE(_) -> failwith "Not Implemented"
     | IfEQ(_) -> failwith "Not Implemented"
