f8e5cef68d7372f6d53e4c9e0432ca67f0395db2
s194042 <hhhermansen@gmail.com>
2024-06-25 14:13:27 +0200

    updates to tests

--- src/Program.fs
+++ src/Program.fs
@@ -69,7 +69,7 @@ let main args =
         let simulation = simulateReationsMatrix initState reactions 0.01
 
 
-        printStateRange simulation 0 40000 ["c";"cnext";"T1";"T2";"T3"]
+        printStateRange simulation 0 40000 ["a";"b";"T1";"T2";"T3"]
 
 
         
