c3bb510747f2d8141caf830fb22c43658bbea3fc
raniiboe <61318713+raniiboe@users.noreply.github.com>
2024-06-26 17:37:15 +0200

    test

--- src/tests.fsx
+++ src/tests.fsx
@@ -607,8 +607,141 @@ let cmpSimWorks (a: int) (b: int) =
                                 (abs(Map.find "Yegtx" res - Yegtx) < 0.1) && (abs(Map.find "Yeltx" res - Yeltx) < 0.1))) false samples
 
 
-let interpreterAndSimAgree (programIdx : int) = 
-    let inputProgram = validPrograms.[abs(programIdx) % (List.length validPrograms)] |> rmws
+
+let validProgramsWithRandomInput = ["crn = {
+    conc[{c},3], conc[cInitial, 3],
+    conc[one,1], conc[zero,0],
+    step[{
+        sub[c,one,cnext],
+        cmp[c,zero]
+    }],
+    step[{
+        ifGT[{ ld[cnext,c] }],
+        ifLE[{ ld[cInitial, c] }]
+    }]
+}"; 
+"crn={
+    conc[{a},20], conc[{b},3], conc[one,1],
+    step[{
+        cmp[a,b]
+    }],
+    step[{
+        ifGE[{
+            sub[a,b,anext],
+            add[q,one,qnext]
+        }]
+    }],
+    step[{
+        ifGE[{
+            ld[anext,a],
+            ld[qnext,q]
+        }],
+        ifLT[{
+            ld[a,r]
+        }]
+    }]
+}";
+"crn={
+    conc[e,1],conc[element,1],
+    conc[divisor,1],conc[one,1],
+    conc[divisorMultiplier,1],
+    step[{
+        div[element,divisor,elementNext],
+        add[divisor,one,divisorNext],
+        add[e,elementNext,eNext]
+    }],
+    step[{
+        ld[elementNext,element],
+        ld[divisorNext,divisor],
+        ld[eNext,e]
+    }]
+}";
+"crn={
+    conc[f,1],conc[one,1],conc[i,5],
+    step[{
+        cmp[i,one],
+        mul[f,i,fnext],
+        sub[i,one,inext]
+    }],
+    step[{
+        ifGT[{
+            ld[inext,i],
+            ld[fnext,f]
+        }]
+    }]
+}";
+"crn = {
+    conc[{a},32],
+    conc[{b},12],
+    step[{
+        ld[a,atmp],
+        ld[b,btmp],
+        cmp[a,b]
+    }],
+    step[{
+        ifGT[{ sub[atmp,btmp,a] }],
+        ifLT[{ sub[btmp,atmp,b] }]
+    }]
+}
+";
+"crn={
+    conc[four,4],conc[divisor1,1],conc[divisor2,3],
+    conc[pi,0],
+    step[{
+        div[four,divisor1,factor1],
+        add[divisor1,four,divisor1Next],
+        div[four,divisor2,factor2],
+        add[divisor2,four,divisor2Next],
+        sub[factor1,factor2,factor],
+        add[pi,factor,piNext]
+    }],
+    step[{
+        ld[divisor1Next,divisor1],
+        ld[divisor2Next,divisor2],
+        ld[piNext,pi]
+    }]
+}";
+"crn = {
+    conc[one,1],conc[n,10],
+    step[{
+        add[z,one,znext],
+        mul[znext,znext,zpow],
+        cmp[zpow,n]
+    }],
+    step[{
+        ifLT[{ld[znext,z]}],
+        ifGE[{ld[z,out]}]
+    }]
+}
+";
+"crn={
+    conc[{a},20],conc[{b},13],
+    conc[one,1],conc[zero,0],
+    step[{
+        cmp[b,zero]
+    }],
+    step[{
+        ifGE[{
+            sub[a,one,anext],
+            sub[b,one,bnext]
+        }]
+    }],
+    step[{
+        ifGE[{
+            ld[anext,a],
+            ld[bnext,b]
+        }]
+    }]
+}
+"
+]
+
+let interpreterAndSimAgree (programIdx : int) (a: int) (b: int) (c: int) =
+    let inputPro = validProgramsWithRandomInput.[abs(programIdx) % (List.length validProgramsWithRandomInput)]
+    let inputProgram = 
+        inputPro.Replace("{a}", a.ToString())
+                .Replace("{b}", b.ToString())
+                .Replace("{c}", c.ToString())
     let ast = 
         match run pprogram inputProgram with
             | Success((res: CRNpp.Root), _, _) -> res
